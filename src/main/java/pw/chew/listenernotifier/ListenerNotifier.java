686d29509efba5670354b05431811f73d9250917
Chew <chew@chew.pw>
2020-07-11 01:40:40 -0500

    Initial files

---
+++ src/main/java/pw/chew/listenernotifier/ListenerNotifier.java
@@ -0,0 +1,14 @@
+package pw.chew.listenernotifier;
+
+public final class ListenerNotifier extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
